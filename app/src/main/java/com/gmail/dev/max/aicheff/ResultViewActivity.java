2d3f0d3835003b2fa83d64eb76360338e9de86c9
Maxim Nebo <gp.max.dev@gmail.com>
2023-04-10 06:31:46 +0300

    Nice refactor main class, work with clean onCreate. split declaration and  initialization of elements. realization of clickListener. all work. need to work on function on click.

--- app/src/main/java/com/gmail/dev/max/aicheff/ResultViewActivity.java
+++ app/src/main/java/com/gmail/dev/max/aicheff/ResultViewActivity.java
@@ -1,13 +1,22 @@
 package com.gmail.dev.max.aicheff;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ResultViewActivity extends AppCompatActivity {
+
+    TextView resultTextView;
+    ImageView resultImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result_activity);
+
+        resultTextView = findViewById(R.id.resultTextView);
+        resultImageView = findViewById(R.id.resultImageView);
     }
 }
