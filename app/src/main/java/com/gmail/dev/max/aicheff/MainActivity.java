686cd98919d565c2f9d8020532d556c0b7dd4319
Maxim Nebo <gp.max.dev@gmail.com>
2023-04-10 05:45:13 +0300

    Nice refactor main class, work with clean onCreate. split declaration and  initialization of elements. realization of clickListener. all work.

--- app/src/main/java/com/gmail/dev/max/aicheff/MainActivity.java
+++ app/src/main/java/com/gmail/dev/max/aicheff/MainActivity.java
@@ -4,60 +4,115 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //private String resultingRequestString = "";
     private StringBuilder resultingRequestStringBuilder = new StringBuilder();
 
+
+    Button okButton, addButton, cancelButton, speechButton, searchButton;
+
+    RadioGroup yesNoBuyingRadioGroup, selectTypeOfDishRadioGroup;
+
+    RadioButton firstCourseRadioButton, mainCourseRadioButton,
+            dessertRadioButton, anyDishRadioButton, yesRadioButton, noRadioButton;
+
+    TextView inputIngredientsTextView; /// ????
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         //manual input text
+        inputIngredientsTextView = findViewById(R.id.editTextTextMultiLine);
+        //buttons
+        searchButton = findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(v -> onClick(v));
 
-        TextView inputIngredientsTextView = findViewById(R.id.editTextTextMultiLine);
+        okButton = findViewById(R.id.okButton);
+        okButton.setOnClickListener(v -> onClick(v));
 
-        Button searchButton = findViewById(R.id.searchButton);
-        searchButton.setOnClickListener(view -> {
-            Intent resultIntent = new Intent(MainActivity.this,
-                    ResultViewActivity.class);
-            startActivity(resultIntent);
-        });
+        addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener(v -> onClick(v));
 
-        Button okButton = findViewById(R.id.okButton);
-        okButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        cancelButton = findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(v -> onClick(v));
 
-                if(inputIngredientsTextView.getText() != ""
-                        && inputIngredientsTextView.getText() != null){
+        speechButton = findViewById(R.id.speechButton);
+        speechButton.setOnClickListener(v -> onClick(v));
 
-                    StringBuilder inputIngredientsTextViewStringBuilder = new StringBuilder();
-                    inputIngredientsTextViewStringBuilder.append(inputIngredientsTextView.getText());
-                    System.out.println(inputIngredientsTextView.getText().toString());// dlya testa
-                    Toast.makeText(getApplicationContext(), inputIngredientsTextView
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.okButton:
+                saveText();
+                break;
+            case R.id.addButton:
+                //addTextToListOfProducts();
+                break;
+
+            case R.id.cancelButton:
+                cancelAddText();
+                break;
+            case R.id.speechButton:
+                //speechAddText();
+                break;
+            case R.id.searchButton:
+                searchTextRequest();
+                break;
+            default:
+                break;
+
+        }
+
+
+    }
+
+
+    private void saveText(){
+
+        if(inputIngredientsTextView.getText() != ""
+                && inputIngredientsTextView.getText() != null){
+
+            StringBuilder inputIngredientsTextViewStringBuilder = new StringBuilder();
+            inputIngredientsTextViewStringBuilder.append(inputIngredientsTextView.getText());
+            System.out.println(inputIngredientsTextView.getText().toString());// dlya testa
+            Toast.makeText(getApplicationContext(), inputIngredientsTextView
                             .getText()
                             .toString(),
-                            Toast.LENGTH_SHORT).show();// dlya testa
+                    Toast.LENGTH_LONG).show();// dlya testa
 
+        }
+    }
 
-                }
+    private void searchTextRequest(){
+        Intent resultIntent = new Intent(MainActivity.this,
+                ResultViewActivity.class);
+        startActivity(resultIntent);
+    }
 
-
-
-            }
-        });
-
-
-
+    private void cancelAddText(){
+        inputIngredientsTextView.setText(R.string.resetToastText);
+        Toast.makeText(getApplicationContext(), inputIngredientsTextView
+                        .getText()
+                        .toString(),
+                Toast.LENGTH_LONG).show();// dlya testa
 
 
 
     }
+
 }
