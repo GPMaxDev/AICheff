2aae4ff73116a991f156c612b739b8033fa351bf
Maxim Nebo <gp.max.dev@gmail.com>
2023-04-10 04:15:56 +0300

    fixed warnings filled string xml not perfect with hint font size in text edit text label 2 activity. no warnings perfect clear. work two buttons. but need refactor.

--- app/src/main/java/com/gmail/dev/max/aicheff/MainActivity.java
+++ app/src/main/java/com/gmail/dev/max/aicheff/MainActivity.java
@@ -2,11 +2,17 @@ package com.gmail.dev.max.aicheff;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private String resultingRequestString = "";
+    private StringBuilder resultingRequestStringBuilder = new StringBuilder();
 
 
 
@@ -15,12 +21,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView inputIngredientsTextView = findViewById(R.id.editTextTextMultiLine);
+
         Button searchButton = findViewById(R.id.searchButton);
         searchButton.setOnClickListener(view -> {
             Intent resultIntent = new Intent(MainActivity.this,
                     ResultViewActivity.class);
             startActivity(resultIntent);
         });
+
+        Button okButton = findViewById(R.id.okButton);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(inputIngredientsTextView.getText() != ""
+                        && inputIngredientsTextView.getText() != null){
+
+                    StringBuilder inputIngredientsTextViewStringBuilder = new StringBuilder();
+                    inputIngredientsTextViewStringBuilder.append(inputIngredientsTextView.getText());
+                    System.out.println(inputIngredientsTextView.getText().toString());// dlya testa
+                    Toast.makeText(getApplicationContext(), inputIngredientsTextView
+                            .getText()
+                            .toString(),
+                            Toast.LENGTH_SHORT).show();// dlya testa
+
+
+                }
+
+
+
+            }
+        });
+
+
+
+
+
 
     }
 }
