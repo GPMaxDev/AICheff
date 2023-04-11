6b0173b0075fade0febe88a73591a258520774f3
Maxim Nebo <gp.max.dev@gmail.com>
2023-04-11 05:14:27 +0300

    work 60% with radio groups, only listener on change button? need reactions

--- app/src/main/java/com/gmail/dev/max/aicheff/MainActivity.java
+++ app/src/main/java/com/gmail/dev/max/aicheff/MainActivity.java
@@ -6,15 +6,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener,
+                    RadioGroup.OnCheckedChangeListener {
 
-    //private String resultingRequestString = "";
-    //private StringBuilder resultingRequestStringBuilder = new StringBuilder();//pozje разделить обьявление и инициализацию
+
+    private StringBuilder resultingRequestStringBuilder;
 
 
     Button okButton, cancelButton, speechButton, searchButton;
@@ -48,6 +51,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         speechButton = findViewById(R.id.speechButton);
         speechButton.setOnClickListener(this::onClick);
 
+        //radioGroup
+        yesNoBuyingRadioGroup = findViewById(R.id.yesNoRadioGroup);
+        yesNoBuyingRadioGroup.setOnCheckedChangeListener(this::onCheckedChanged);
+
+        selectTypeOfDishRadioGroup = findViewById(R.id.selectTypeOfDishRadioGroup);
+        selectTypeOfDishRadioGroup.setOnCheckedChangeListener(this::onCheckedChanged);
+
+        //string builder
+
+        resultingRequestStringBuilder = new StringBuilder();
+
 
     }
 
@@ -77,35 +91,80 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+        RadioButton checkedRadioButton = findViewById(i);
+
+
+        switch (checkedRadioButton.getId()){
+
+            case R.id.yesRadioButton:
+                Toast.makeText(getApplicationContext(), "YEs",
+                        Toast.LENGTH_LONG).show();
+                break;
+            case R.id.noRadioButton:
+                Toast.makeText(getApplicationContext(), "NO",
+                        Toast.LENGTH_LONG).show();
+                break;
+            case R.id.firstCourseRadioButton:
+                Toast.makeText(getApplicationContext(), "first",
+                        Toast.LENGTH_LONG).show();
+                break;
+            case R.id.mainCourseRadioButton:
+                Toast.makeText(getApplicationContext(), "Main",
+                        Toast.LENGTH_LONG).show();
+                break;
+            case R.id.dessertRadioButton:
+                Toast.makeText(getApplicationContext(), "dessert",
+                        Toast.LENGTH_LONG).show();
+                break;
+            case R.id.anyDishRadioButton:
+                Toast.makeText(getApplicationContext(), "Any",
+                        Toast.LENGTH_LONG).show();
+                break;
+            default:
+                break;
+
+
+        }
+
+
+
+    }
+
 
     private void saveText(){
 
         if(inputIngredientsTextView.getText() != ""
                 && inputIngredientsTextView.getText() != null){
 
-            resultInputInTextView.setText(R.string.sumProductListHeaderString);
+            resultInputInTextView.setText(R.string.your_product_list);
             listOfProductsTextView.setText(inputIngredientsTextView.getText());
-            Toast.makeText(getApplicationContext(), R.string.successAddProductsToList,
+            resultingRequestStringBuilder.setLength(0);
+            resultingRequestStringBuilder.append(inputIngredientsTextView.getText());
+            Toast.makeText(getApplicationContext(), R.string.success_add_products_to_list,
                     Toast.LENGTH_SHORT).show();
 
         }
     }
-
-
-    private void searchTextRequest(){
-        //dobavit peredachu dannih na druguu activity cherez intent
-
-        Intent resultIntent = new Intent(MainActivity.this,
-                ResultViewActivity.class);
-        startActivity(resultIntent);
-    }
-
     private void cancelAddText(){
         inputIngredientsTextView.setText("");
         listOfProductsTextView.setText("");
-        resultInputInTextView.setText(R.string.sumProductListHeaderString);
 
-         Toast.makeText(getApplicationContext(), R.string.resetToastText,
+        resultInputInTextView.setText(R.string.your_product_list);
+
+         Toast.makeText(getApplicationContext(), R.string.reset_toast_text,
                 Toast.LENGTH_SHORT).show();
     }
+
+    private void searchTextRequest(){
+
+        Intent resultIntent = new Intent(MainActivity.this,
+                ResultViewActivity.class);
+        resultIntent.putExtra("searchRequest",resultingRequestStringBuilder.toString());
+        startActivity(resultIntent);
+    }
+
+
 }
--- app/src/main/java/com/gmail/dev/max/aicheff/ResultViewActivity.java
+++ app/src/main/java/com/gmail/dev/max/aicheff/ResultViewActivity.java
@@ -1,5 +1,6 @@
 package com.gmail.dev.max.aicheff;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,12 +12,22 @@ public class ResultViewActivity extends AppCompatActivity {
     TextView resultTextView;
     ImageView resultImageView;
 
+    Intent intent;
+
+    String finalRequestString;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result_activity);
 
+
         resultTextView = findViewById(R.id.resultTextView);
         resultImageView = findViewById(R.id.resultImageView);
+        intent = getIntent();
+        finalRequestString = intent.getStringExtra("searchRequest");
+
+        resultTextView.setText(finalRequestString);
+
     }
 }
